dd40846c2b94aae886bd6c2bd1000599a97f5bc9
bradtxufl <brad.w@utexas.edu>
2019-05-29 14:13:19 -0700

    part 2 minus stretch

--- part2/server.js
+++ part2/server.js
@@ -1,9 +1,38 @@
 var express = require('express');
 var app = express();
 var port = process.env.PORT || 8000;
+var fs = require('fs')
 
 app.get('/yourroute', function(req, res) {
   res.send("stuff");
+});
+
+app.post('/create/:name/:age', function(req,res) {
+  var thingy = {
+    'name': req.params.name,
+    'age': req.params.age
+  }
+  let current = fs.readFileSync(`./part2/storage.json`)
+  current = JSON.parse(current)
+  current.push(thingy)
+  fs.writeFileSync(`./part2/storage.json`, JSON.stringify(current))
+  res.json(current)
+});
+
+app.get('/', function (req, res) {
+  res.sendFile(`${__dirname}/storage.json`)
+});
+
+app.get('/:name', function (req, res) {
+  var info = fs.readFileSync(`${__dirname}/storage.json`)
+  arr = JSON.parse(info)
+  result = 400
+  for (var i = 0; i < arr.length; i++) {
+    if (arr[i].name === req.params.name) {
+      result = arr[i]
+    }
+    res.send(result)
+  }
 });
 
 app.use(function(req, res) {
