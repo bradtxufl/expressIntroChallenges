c3aa7ea24793ef4d8a600613d5dd4b33c44660aa
bradtxufl <brad.w@utexas.edu>
2019-05-29 11:31:30 -0700

    part1 es done

--- part1/server.js
+++ part1/server.js
@@ -1,10 +1,35 @@
 var express = require('express');
 var app = express();
 var port = process.env.PORT || 8000;
+var fs = require('fs')
 
 app.get('/yourroute', function(req, res) {
   res.send("stuff");
 });
+
+app.get('/hello', function(req, res) {
+  res.send('Hello');
+});
+
+app.post('/create/:name', function(req, res) {
+  let thingy = {
+    'id': 1,
+    'name': req.params.name
+  };
+  res.json(thingy)
+});
+
+app.get('/', function(req, res) {
+  res.sendFile(`${__dirname}/index.html`)
+})
+
+app.get('/verify/:age', function(req, res) {
+  if(req.params.age > 13) {
+    res.sendStatus(200)
+  } else {
+    res.sendStatus(403)
+  }
+})
 
 app.use(function(req, res) {
   res.sendStatus(404);
